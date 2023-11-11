db34200586eb61cbb2c5f2b90696558cede28a16
Akayy <techakayy@gmail.com>
2023-11-12 10:11:43 +1100

    first commit

---
+++ src/components/SampleCard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+	import { ref } from 'vue'
+
+	const props = defineProps({
+		name: {
+			type: String,
+			default: 'Component' /*PinegrowComponentTemplate*/,
+		},
+	})
+
+	const suffix = ref('placeholder')
+</script>
+<template>
+    <div class="p-4">
+        <div class="max-w-sm rounded overflow-hidden shadow-lg mb-4">
+            <img class="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains"/>
+            <div class="px-6 py-4 pb-2">
+                <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+                <p class="text-gray-700 text-base">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
+					perferendis eaque, exercitationem praesentium nihil. </p>
+            </div>
+            <div class="px-6 py-4"><span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 hover:bg-gray-300 mr-2">#photography</span><span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 hover:bg-gray-300 mr-2">#travel</span><span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 hover:bg-gray-300">#winter</span>
+            </div>
+        </div><a href="/about"> <button class="bg-primary-900 font-bold hover:bg-blue-700 px-4 py-2 rounded text-fuchsia-500">
+                <slot>Button</slot>
+            </button> </a>
+    </div>
+</template>
+<style scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('@pinegrow/tailwindcss-plugin')} */
+const {
+  pg_colors,
+  pg_fonts,
+  pg_backgrounds,
+} = require('./themes/pg-tailwindcss/tokens.cjs') // tailwind.config.[js,cjs]
+// import { pg_colors, pg_fonts, pg_backgrounds } from './themes/pg-tailwindcss/tokens.mjs' // tailwind.config.[ts]
+
+export default {
+  /* Please ensure that you update the filenames and paths to accurately match those used in your project. */
+  get content() {
+    const _content = [
+      './index.html',
+      './src/**/*.{html,vue,svelte,astro,js,cjs,ts,cts,mts,jsx,tsx,md,mdx}',
+      //...
+    ]
+    return process.env.NODE_ENV === 'production'
+      ? _content
+      : [..._content, './_pginfo/**/*.{html,js}'] // used by Vue Desginer for live-designing during development
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@pinegrow/tailwindcss-plugin').default({
+      colors: pg_colors, // primary, secondary etc
+      fonts: pg_fonts,
+      backgrounds: pg_backgrounds, // bg-design-image, bg-design-image-large
+    }),
+  ],
+}
